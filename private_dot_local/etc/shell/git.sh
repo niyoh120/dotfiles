d6ffc4812c4ad9322e3ff02797cdbba5336754e2
niyoh <zhrh120@gmail.com>
2023-01-06 14:07:55 +0800

    feat: add git helper

---
+++ private_dot_local/etc/shell/git.sh
@@ -0,0 +1,8 @@
+# git
+
+# list any files flagged w/ --skip-worktree alias
+alias glsskip="git ls-files -v|grep '^S'"
+# add --skip-worktree flag to file
+gskip() {  git update-index --skip-worktree "$@";  git status; }
+# remove --skip-worktree flag from file
+gunskip() {  git update-index --no-skip-worktree "$@";  git status; }
