6173976e99e3187cfc324cb9f0fbb1c5cf46c42d
niyoh <zhrh120@gmail.com>
2024-03-09 23:54:58 +0800

    refactor(check): check at shell boot

---
+++ private_dot_local/etc/shell/check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+function check_cmd() {
+    [ ! "$(command -v "$1")" ] && echo "$1 not found!"
+}
+
+
+
+function check_all() {
+    if [ -v SHELL_SKIP_CHECK_CMD ]; then
+        return
+    fi
+    check_cmd git
+    check_cmd fzf
+    check_cmd duf
+    check_cmd asdf
+    check_cmd starship
+    check_cmd exa
+    check_cmd fd
+    check_cmd thefuck
+
+    check_cmd python3
+    check_cmd pip3
+    check_cmd pipx
+    check_cmd prettyping
+}
+
+check_all
