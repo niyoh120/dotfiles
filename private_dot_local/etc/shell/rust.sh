0393eb7f5aec389f5905dc228cc42ac25671d1b0
niyoh <zhrh120@gmail.com>
2024-04-30 16:48:40 +0800

    fix(rush.sh): fix bin

--- private_dot_local/etc/shell/rust.sh
+++ private_dot_local/etc/shell/rust.sh
@@ -4,6 +4,8 @@ _cargo_path="$HOME/.cargo"
 
 [ -e "$_cargo_path/env" ] && source "$_cargo_path/env"
 
+push_back_path "$_cargo_path/bin"
+
 # use ustc mirror
 export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
 export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
