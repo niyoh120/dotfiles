06c3f99481aff1cf862f3f64483ca955da2b502f
niyoh <zhrh120@gmail.com>
2024-07-24 08:50:43 +0800

    perf: Update.

--- private_dot_local/etc/shell/check.sh
+++ private_dot_local/etc/shell/check.sh
@@ -3,15 +3,12 @@ function check_cmd() {
     [ ! "$(command -v "$1")" ] && echo "$1 not found!"
 }
 
-
-
 function check_all() {
     if [ -v SHELL_SKIP_CHECK_CMD ]; then
         return
     fi
     check_cmd git
     check_cmd fzf
-    check_cmd duf
     check_cmd asdf
     check_cmd starship
     check_cmd exa
