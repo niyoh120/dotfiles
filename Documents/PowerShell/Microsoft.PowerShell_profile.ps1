9557deaf5315f9ce6a88ef54beadf06eacf0e4d2
niyoh <zhrh120@gmail.com>
2024-08-15 11:14:21 +0800

    Update.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,5 @@
 kubectl completion powershell | Out-String | Invoke-Expression
 helm completion powershell | Out-String | Invoke-Expression
-zoxide init powershell | Out-String | Invoke-Expression
 
 if (Get-Module -ListAvailable -Name PSReadLine) {
     Import-Module PSReadLine
@@ -8,3 +7,6 @@ if (Get-Module -ListAvailable -Name PSReadLine) {
 }
 
 oh-my-posh init pwsh | Invoke-Expression
+
+# 必须放最后
+zoxide init powershell | Out-String | Invoke-Expression
