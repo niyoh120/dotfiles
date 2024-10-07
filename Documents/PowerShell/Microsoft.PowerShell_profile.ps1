237409d6001d6aeac2eee8488209fa8e82a09b6c
niyoh <zhrh120@gmail.com>
2024-10-07 23:51:52 +0800

    移除kubectl和helm的自动补全脚本
    从Microsoft.PowerShell_profile.ps1中删除了kubectl和helm的自动补全脚本。现在只保留了PSReadLine模块的导入和配置，以提升Shell的编辑体验。

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,3 @@
-kubectl completion powershell | Out-String | Invoke-Expression
-helm completion powershell | Out-String | Invoke-Expression
-
 if (Get-Module -ListAvailable -Name PSReadLine) {
     Import-Module PSReadLine
     Set-PSReadLineOption -EditMode Emacs
