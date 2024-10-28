769158949bfbceabf5613e8b7b35ae7529fb0c60
niyoh <zhrh120@gmail.com>
2024-10-28 16:21:00 +0800

    feat(dot_zshrc): 添加 sgpt 模块并配置 Shell-GPT 集成
    - 在 dot_zshrc 文件中添加 sgpt 模块
    - 新增 sgpt.sh 文件实现 Shell-GPT 集成
    - 绑定 Ctrl+Q 快捷键以激活 Shell-GPT

--- private_dot_local/etc/shell/sgpt.sh
+++ private_dot_local/etc/shell/sgpt.sh
@@ -1,13 +1,15 @@
-# Shell-GPT integration ZSH v0.2
-_sgpt_zsh() {
-if [[ -n "$BUFFER" ]]; then
-    _sgpt_prev_cmd=$BUFFER
-    BUFFER+="⌛"
-    zle -I && zle redisplay
-    BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
-    zle end-of-line
+if [ "$(command -v sgpt)" ]; then
+    # Shell-GPT integration ZSH v0.2
+    _sgpt_zsh() {
+    if [[ -n "$BUFFER" ]]; then
+        _sgpt_prev_cmd=$BUFFER
+        BUFFER+="⌛"
+        zle -I && zle redisplay
+        BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
+        zle end-of-line
+    fi
+    }
+    zle -N _sgpt_zsh
+    bindkey ^q _sgpt_zsh
+    # Shell-GPT integration ZSH v0.2
 fi
-}
-zle -N _sgpt_zsh
-bindkey ^l _sgpt_zsh
-# Shell-GPT integration ZSH v0.2
